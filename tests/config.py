d9b79e010b698628fb83a2f398043962e614988e
leobratisman <leonhard11@yandex.ru>
2026-03-03 21:19:25 +0300

    4.4 Конфигурация тестового окружения. Практическое задание: конфигурация Postgres для тестового окружения

--- tests/config.py
+++ tests/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from tests.tools.config.grpc import GRPCClientTestConfig, GRPCServerTestConfig
 from tests.tools.config.http import HTTPClientTestConfig, HTTPServerTestConfig
 from tests.tools.config.kafka import KafkaClientTestConfig
+from tests.tools.config.postgres import PostgresClientTestConfig
 
 
 class TestSettings(BaseSettings):
@@ -22,6 +23,7 @@ class TestSettings(BaseSettings):
     operations_http_client: HTTPClientTestConfig
     operations_grpc_client: GRPCClientTestConfig
     operations_kafka_client: KafkaClientTestConfig
+    operations_postgres_client: PostgresClientTestConfig
 
     operations_processing_wait_timeout: float
 
