17999d953b7b3e07468a345c302a963b63b8a53a
leobratisman <leonhard11@yandex.ru>
2026-03-08 13:58:07 +0300

    7.4 Сценарные тесты gRPC API. Практическое задание: Сценарный gRPC тест gateway-service

--- tests/assertions/grpc/gateway.py
+++ tests/assertions/grpc/gateway.py
@@ -7,8 +7,8 @@ from protos.gen.contracts.services.gateway.user_details_pb2 import UserDetails
 from protos.gen.contracts.services.gateway.rpc_get_user_details_pb2 import GetUserDetailsResponse
 from protos.gen.contracts.services.gateway.rpc_get_account_details_pb2 import GetAccountDetailsResponse
 from protos.gen.contracts.services.users.user_pb2 import User
-from protos.gen.contracts.services.accounts.account_pb2 import Account
-from protos.gen.contracts.services.cards.card_pb2 import Card
+from protos.gen.contracts.services.accounts.account_pb2 import Account, AccountType, AccountStatus
+from protos.gen.contracts.services.cards.card_pb2 import Card, CardType, CardStatus, CardPaymentSystem
 
 from tests.assertions.base import assert_equal
 from tests.assertions.grpc.accounts import assert_account
@@ -16,8 +16,6 @@ from tests.assertions.grpc.cards import assert_card
 from tests.assertions.grpc.users import assert_user
 from tests.tools.logger import get_test_logger
 from tests.tools.date import to_proto_test_date
-from tests.types.accounts import AccountTestType, AccountTestStatus
-from tests.types.cards import CardTestPaymentSystem, CardTestStatus, CardTestType
 
 logger = get_test_logger("GATEWAY_ASSERTIONS")
 
@@ -83,9 +81,9 @@ def assert_get_user_details_response_user_with_active_credit_card_account(
             accounts=[
                 Account(
                     id="99999999-aaaa-4bbb-8ccc-000000000001",
-                    type=AccountTestType.CREDIT_CARD,
-                    status=AccountTestStatus.ACTIVE,
-                    user_id="3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                    type=AccountType.ACCOUNT_TYPE_CREDIT_CARD,
+                    status=AccountStatus.ACCOUNT_STATUS_ACTIVE,
+                    user_id="8b0e7c2a-1b6a-4e5d-9f1a-1b3f2a7c9e21",
                     balance=-15230.75,
                 )
             ],
@@ -103,36 +101,36 @@ def assert_get_account_details_response_user_with_active_debit_card_account(
     expected = GetAccountDetailsResponse(
         details=AccountDetails(
             account=Account(
-                id="99999999-aaaa-4bbb-8ccc-000000000001",
-                type=AccountTestType.DEBIT_CARD,
-                status=AccountTestStatus.ACTIVE,
-                user_id="3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                balance=-15230.75,
+                id="10000000-aaaa-4bbb-8ccc-000000000002",
+                type=AccountType.ACCOUNT_TYPE_DEBIT_CARD,
+                status=AccountStatus.ACCOUNT_STATUS_ACTIVE,
+                user_id="8b0e7c2a-1d3a-4e5d-9f1a-1b3f2a9c9e31",
+                balance=4000.75,
             ),
             cards=[
                 Card(
-                    id="11111111-aaaa-4bbb-8ccc-222222222222",
+                    id="11111111-aaaa-4bbb-8ccc-222222222223",
                     pin="1234",
                     cvv="456",
-                    type=CardTestType.VIRTUAL,
-                    status=CardTestStatus.ACTIVE,
-                    account_id="aaaaaaaa-bbbb-4ccc-8ddd-eeeeeeeeeeee",
+                    type=CardType.CARD_TYPE_VIRTUAL,
+                    status=CardStatus.CARD_STATUS_ACTIVE,
+                    account_id="10000000-aaaa-4bbb-8ccc-000000000002",
                     card_number="4111111111111111",
                     card_holder="IVAN PETROV",
                     expiry_date=to_proto_test_date(date(2027, 12, 31)),
-                    payment_system=CardTestPaymentSystem.VISA,
+                    payment_system=CardPaymentSystem.CARD_PAYMENT_SYSTEM_VISA,
                 ),
                 Card(
-                    id="33333333-dddd-4eee-8fff-444444444444",
+                    id="33333333-dddd-4eee-8fff-444444444445",
                     pin="9876",
                     cvv="789",
-                    type=CardTestType.PHYSICAL,
-                    status=CardTestStatus.ACTIVE,
-                    account_id="aaaaaaaa-bbbb-4ccc-8ddd-eeeeeeeeeeee",
+                    type=CardType.CARD_TYPE_PHYSICAL,
+                    status=CardStatus.CARD_STATUS_ACTIVE,
+                    account_id="10000000-aaaa-4bbb-8ccc-000000000002",
                     card_number="5500000000000004",
                     card_holder="IVAN PETROV",
                     expiry_date=to_proto_test_date(date(2028, 6, 30)),
-                    payment_system=CardTestPaymentSystem.MASTERCARD,
+                    payment_system=CardPaymentSystem.CARD_PAYMENT_SYSTEM_MASTERCARD,
                 ),
             ],
         )
